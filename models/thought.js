6e101645610e4e6bceaaf7bdb3d8e8bb5eacc266
Jesus <jesusbalandra@outlook.com>
2022-03-27 17:38:29 -0600

    created thought model, and used dateFormat util

---
+++ models/thought.js
@@ -0,0 +1,28 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const thoughtSchema = new Schema ({
+    thoughtText: {
+        type: String,
+        required: true,
+        maxLength: 280,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => dateFormat(createdAtVal)
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    reactions: [reactionSchema],
+});
+
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
