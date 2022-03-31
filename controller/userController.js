c97f7c9c7a0f820297e948ccc6727d81ef462d42
Jesus <jesusbalandra@outlook.com>
2022-03-31 14:32:37 -0600

    created userController

---
+++ controller/userController.js
@@ -0,0 +1,66 @@
+const { User } = require('../models');
+
+module.exports = {
+    getAllUsers: async (req, res) => {
+        try {
+            const users = await User.find({});
+            res.json(users);
+        } catch (e) {
+            res.json(e);
+        }
+    },
+    getUserById: async (req, res) => {
+        const { userId } = req.params;
+        try {
+            const user = await User.findById(userId)
+            .populate({
+                path: 'thoughts',
+            })
+            .populate({
+                path: 'friends',
+            });
+            res.json(user);
+        } catch (e) {
+            res.json(e);
+        }
+    },
+    createUser: async (req, res) => {
+        const {
+            username,
+            email,
+        } = req.body;
+        try {
+            const newUser = await User.create({
+                username,
+                email,
+            });
+            res.json(newUser);
+        } catch (e) {
+            res.json(e);
+        }
+    },
+    updateUserById: async (req, req) => {
+        const {userId} = req.params;
+        try {
+            const updateUser = await User.findByIdAndUpdate(
+                userId,
+                {...req.body},
+                {
+                    new: true,
+                    runValidators: true,
+                }
+                );
+        } catch (e) {
+            res.json(e);
+        }
+    },
+    deleteUserById: async (req,res) => {
+        const { userId } = req.params;
+        try {
+            const deletedUser = await User.findByIdAndDelete(userId);
+            res.json(deletedUser);
+        } catch (e) {
+            res.json(e);
+        }
+    },
+};
--- models/user.js
+++ models/user.js
@@ -21,6 +21,10 @@ const userSchema = new Schema({
         type: Schema.Types.ObjectId,
         ref: 'User',
     }],
+}, {
+    toJSON: {
+        virtuals: true,
+    }
 });
 
 userSchema.virtual('friendCount').get(function() {
